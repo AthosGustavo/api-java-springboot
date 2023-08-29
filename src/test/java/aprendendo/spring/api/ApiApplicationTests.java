95b97ba00edeeea826dfc45b4d5a71e319e2143f
Athos <athosgustavowf@gmail.com>
2023-08-29 19:05:25 -0300

    criando projeto

---
+++ src/test/java/aprendendo/spring/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package aprendendo.spring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
