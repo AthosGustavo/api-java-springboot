d93183bb88265d5485fe4f0f3267fd00d1aadf0d
Athos <athosgustavowf@gmail.com>
2023-08-29 22:24:17 -0300

    Criação das entidades professor, aluno e disciplina

---
+++ src/main/java/aprendendo/spring/api/Aluno/Aluno.java
@@ -0,0 +1,30 @@
+package aprendendo.spring.api.Aluno;
+
+
+import aprendendo.spring.api.Disciplina.Disciplina;
+import jakarta.persistence.*;
+import lombok.Getter;
+import java.util.List;
+
+@Getter
+@Entity
+@Table(name="alunos")
+public class Aluno{
+
+    //o spring entende camel case e converte para snake case
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id_aluno")
+    private Long idAluno;
+
+    private String nomeAluno;
+
+    private Integer idadeAluno;
+
+    @ManyToMany
+    @JoinTable(name="aluno_disciplina",
+    joinColumns = @JoinColumn(name="id_aluno"),
+    inverseJoinColumns = @JoinColumn(name="id_disciplina"))
+    private List<Disciplina> listaDisciplina;
+}
